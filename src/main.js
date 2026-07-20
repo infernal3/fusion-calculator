c2d7ff8e5cd8183d746ee66e16539b9f74cf0736
infernal3 <infernal1559@gmail.com>
2026-07-20 15:43:10 -0500

    fix: text formatting with prettier

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const el = e => getElementById(e);
+const el = (e) => getElementById(e);
 var specialFusionRecipes = [
     // Fusion recipes for Common Shards
     { id: "C1", predicate: (A, B) => A.shardCategory == "Forest" && A.shardID.slice(0, 1) == "C" && getRarityIndex(B.shardID.slice(0, 1)) >= 2 },
@@ -95,7 +95,7 @@ var specialFusionRecipes = [
 ];
 
 // ShardA and ShardB should be Shard ID, not the shard name or the processed shard object.
-var calculateFusionResult = function(ShardTable, ShardA, ShardB) {
+var calculateFusionResult = function (ShardTable, ShardA, ShardB) {
     // Chameleon-fusion overrides all other fusion type. Check for the presence of a Chameleon:
     if (ShardA == "L4") {
         return calculateChameleonFusionResult(ShardTable, ShardB);
@@ -103,7 +103,7 @@ var calculateFusionResult = function(ShardTable, ShardA, ShardB) {
     if (ShardB == "L4") {
         return calculateChameleonFusionResult(ShardTable, ShardA);
     }
-    
+
     // candidateA and candidateB are the ID Fusion results for Shard A and Shard B:
     var candidateA = calculateIDFusionResult(ShardTable, ShardA);
     var candidateB = calculateIDFusionResult(ShardTable, ShardB);
@@ -112,46 +112,49 @@ var calculateFusionResult = function(ShardTable, ShardA, ShardB) {
     var specialCandidates = calculateAllSpecialFusions(ShardTable, ShardA, ShardB);
 
     // Merge Special Fusion and ID Fusion candidates into one array. Purge duplicates and invalid results:
-    var candidates = Array.from(new Set(specialCandidates.concat(candidateA, candidateB))).filter((item) => {
-        return item != "empty slot";
-    }).filter((item) => {
-        return item.shardID != ShardA;
-    }).filter((item) => {
-        return item.shardID != ShardB;
-    });
-    
+    var candidates = Array.from(new Set(specialCandidates.concat(candidateA, candidateB)))
+        .filter((item) => {
+            return item != "empty slot";
+        })
+        .filter((item) => {
+            return item.shardID != ShardA;
+        })
+        .filter((item) => {
+            return item.shardID != ShardB;
+        });
+
     // If there are more than 3 candidates remaining after purging, omit lower priority candidates:
     while (candidates.length > 3) {
         candidates.pop();
     }
     return candidates;
-}
+};
 
-var calculateAllSpecialFusions = function(ShardTable, ShardA, ShardB) {
+var calculateAllSpecialFusions = function (ShardTable, ShardA, ShardB) {
     // Fetch rarity, family, category, name for both of the shards:
     var ShardObjectA = ShardTable[getRarityCuteName(ShardA.slice(0, 1))][parseInt(ShardA.slice(1))];
     var ShardObjectB = ShardTable[getRarityCuteName(ShardB.slice(0, 1))][parseInt(ShardB.slice(1))];
     var array = [];
-    
+
     // Iterate through all of the special fusion recipes and check if any apply
     specialFusionRecipes.forEach((recipe) => {
-        if(recipe.predicate(ShardObjectA, ShardObjectB)) {
+        if (recipe.predicate(ShardObjectA, ShardObjectB)) {
             array.push(recipe.id);
             return;
         }
-        if(recipe.predicate(ShardObjectB, ShardObjectA)) {
+        if (recipe.predicate(ShardObjectB, ShardObjectA)) {
             array.push(recipe.id);
             return;
         }
     });
     // Sort the array by Shard ID (Highest to lowest).
     return array.toReversed().map((item) => {
-        return {shardID: item, amount: 2};
+        return { shardID: item, amount: 2 };
     });
-}
+};
 
-var getNextTierShardLetter = function(ShardLetter) {
-    switch(ShardLetter) {
+var getNextTierShardLetter = function (ShardLetter) {
+    switch (ShardLetter) {
         case "C":
             return "U";
         case "U":
@@ -163,10 +166,10 @@ var getNextTierShardLetter = function(ShardLetter) {
         default:
             return "Z";
     }
-}
+};
 
-var getRarityCuteName = function(ShardLetter) {
-    switch(ShardLetter) {
+var getRarityCuteName = function (ShardLetter) {
+    switch (ShardLetter) {
         case "C":
             return "common";
         case "U":
@@ -180,10 +183,10 @@ var getRarityCuteName = function(ShardLetter) {
         default:
             return "unsorted";
     }
-}
+};
 
-var getRarityIndex = function(ShardLetter) {
-    switch(ShardLetter) {
+var getRarityIndex = function (ShardLetter) {
+    switch (ShardLetter) {
         case "C":
             return 1;
         case "U":
@@ -197,9 +200,9 @@ var getRarityIndex = function(ShardLetter) {
         default:
             return -1;
     }
-}
+};
 
-var calculateIDFusionResult = function(ShardTable, Shard) {
+var calculateIDFusionResult = function (ShardTable, Shard) {
     var ShardLetter = Shard.slice(0, 1);
     var ShardNumber = parseInt(Shard.slice(1));
     var ShardTableOfRarity = ShardTable[getRarityCuteName(ShardLetter)];
@@ -210,96 +213,106 @@ var calculateIDFusionResult = function(ShardTable, Shard) {
     while (true) {
         index++;
         if (ShardTableOfRarity[index] == undefined || index >= 200) return "empty slot";
-        if (ShardCategory == ShardTableOfRarity[index].shardCategory) return {shardID: ShardLetter + index, amount: 1};
+        if (ShardCategory == ShardTableOfRarity[index].shardCategory) return { shardID: ShardLetter + index, amount: 1 };
     }
-}
+};
 
-var calculateChameleonFusionResult = function(ShardTable, NonChameleonShard) {
+var calculateChameleonFusionResult = function (ShardTable, NonChameleonShard) {
     var ShardLetter = NonChameleonShard.slice(0, 1);
     var ShardNumber = parseInt(NonChameleonShard.slice(1));
     var candidate1 = ShardLetter + (ShardNumber + 1);
     var candidate2 = ShardLetter + (ShardNumber + 2);
     var candidate3 = ShardLetter + (ShardNumber + 3);
     var missingCandidates = 0;
-    var validate = function(candidate) {
-        if(checkIfShardIDExists(ShardTable, candidate)) {
+    var validate = function (candidate) {
+        if (checkIfShardIDExists(ShardTable, candidate)) {
             return candidate;
         }
-        return getNextTierShardLetter(ShardLetter) + (++missingCandidates);
-    }
-    return [{
-        shardID: validate(candidate1), 
-        amount: 1,
-    }, {
-        shardID: validate(candidate2), 
-        amount: 1,
-    }, {
-        shardID: validate(candidate3), 
-        amount: 1,
-    }].filter((item) => {
-        return !item.shardID.includes("Z");
-    }).filter((item) => {
-        return item.shardID != "L4"; // Chameleon Fusion will never output a Chameleon
-    }).filter((item) => {
-        return item.shardID != NonChameleonShard; // Chameleon Fusion will never output the non-chameleon shard used
-    });
-}
+        return getNextTierShardLetter(ShardLetter) + ++missingCandidates;
+    };
+    return [
+        {
+            shardID: validate(candidate1),
+            amount: 1,
+        },
+        {
+            shardID: validate(candidate2),
+            amount: 1,
+        },
+        {
+            shardID: validate(candidate3),
+            amount: 1,
+        },
+    ]
+        .filter((item) => {
+            return !item.shardID.includes("Z");
+        })
+        .filter((item) => {
+            return item.shardID != "L4"; // Chameleon Fusion will never output a Chameleon
+        })
+        .filter((item) => {
+            return item.shardID != NonChameleonShard; // Chameleon Fusion will never output the non-chameleon shard used
+        });
+};
 
-var checkIfShardIDExists = function(ShardTable, ShardID) {
+var checkIfShardIDExists = function (ShardTable, ShardID) {
     // might optimize this if necessary. but my array only has a 400-ish elements. hopefully this doesn't bite me in the back.
     var flag = false;
-    ShardTable.sorted.forEach(item => {
-        if(!item || item == null) return;
-        if(item.shardID == ShardID) flag = true;
+    ShardTable.sorted.forEach((item) => {
+        if (!item || item == null) return;
+        if (item.shardID == ShardID) flag = true;
     });
     return flag;
-}
+};
 
-var loadOneShardConstant = async function(resource) {
+var loadOneShardConstant = async function (resource) {
     var response = await fetch(new Request(resource));
     var json = await response.json();
     return json;
-}
+};
 
-var loadManyShardConstants = async function() {
+var loadManyShardConstants = async function () {
     var common = await loadOneShardConstant("../src/common.json");
     var uncommon = await loadOneShardConstant("../src/uncommon.json");
     var rare = await loadOneShardConstant("../src/rare.json");
     var epic = await loadOneShardConstant("../src/epic.json");
     var legendary = await loadOneShardConstant("../src/legendary.json");
     return {
-        common: common, 
-        uncommon: uncommon, 
-        rare: rare, 
-        epic: epic, 
+        common: common,
+        uncommon: uncommon,
+        rare: rare,
+        epic: epic,
         legendary: legendary,
         unsorted: [].concat(common, uncommon, rare, epic, legendary),
     };
-}
+};
 
 var Loading = true;
 var ShardTable = {};
 
-loadManyShardConstants().then(result => {
-    ShardTable = result;
-}).then(() => {
-    ShardTable.sorted = ShardTable.unsorted.toSorted((a, b) => {
-        if (a == null) {
-            return 1;
-        }
-        if (b == null) {
-            return -1;
-        }
-        var genericSort = [a.shardName, b.shardName].sort();
+loadManyShardConstants()
+    .then((result) => {
+        ShardTable = result;
+    })
+    .then(() => {
+        ShardTable.sorted = ShardTable.unsorted.toSorted((a, b) => {
+            if (a == null) {
+                return 1;
+            }
+            if (b == null) {
+                return -1;
+            }
+            var genericSort = [a.shardName, b.shardName].sort();
 
-        if (genericSort[0] == genericSort[1]) {
-            return 0;
-        }
-        if (genericSort[0] == a.shardName) {
-            return -1;
-        }
-        return 1;
+            if (genericSort[0] == genericSort[1]) {
+                return 0;
+            }
+            if (genericSort[0] == a.shardName) {
+                return -1;
+            }
+            return 1;
+        });
+    })
+    .then(() => {
+        Loading = false;
     });
-}).then(() => {
-    Loading = false;
-});
