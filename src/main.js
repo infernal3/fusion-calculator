d1d648f481be6c4a6378f1761f4093d880821150
infernal3 <infernal1559@gmail.com>
2026-07-20 15:06:10 -0500

    feat: add all the (known) fusion recipes, up to July 20, 2026.

--- src/main.js
+++ src/main.js
@@ -40,6 +40,7 @@ var calculateFusionResult = function(ShardTable, ShardA, ShardB) {
         {id: "U22", predicate: (A, B) => A.shardName == "Pest" && B.shardCategory == "Combat"},
         {id: "U34", predicate: (A, B) => A.shardFamily == "Bird" && getRarityIndex(B.shardID.slice(0, 1)) >= 2},
         {id: "U39", predicate: (A, B) => A.shardFamily == "Frog" && B.shardCategory == "Combat"},
+        {id: "U40", predicate: (A, B) => A.shardFamily == "Bug" && B.shardSkill == "Mining"},
         {id: "U41", predicate: (A, B) => A.shardCategory == "Water" && A.shardID.slice(0, 1) == "U" && getRarityIndex(B.shardID.slice(0, 1)) >= 1},
         // Fusion recipes for Rare Shards
         {id: "R1", predicate: (A, B) => A.shardCategory == "Forest" && A.shardID.slice(0, 1) == "R" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
@@ -51,6 +52,64 @@ var calculateFusionResult = function(ShardTable, ShardA, ShardB) {
         {id: "R9", predicate: (A, B) => A.shardName == "Viper" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 2},
         {id: "R13", predicate: (A, B) => A.shardFamily == "Shulker" && B.shardName == "Wither"},
         {id: "R15", predicate: (A, B) => A.shardName == "Lapis Zombie" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "R16", predicate: (A, B) => A.shardName == "Termite" && B.shardName == "Praying Mantis"},
+        {id: "R22", predicate: (A, B) => A.shardFamily == "Shulker" && B.shardFamily == "Demon"},
+        {id: "R30", predicate: (A, B) => A.shardName == "Lapis Skeleton" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "R33", predicate: (A, B) => A.shardName == "Troglobyte" && (B.shardFamily == "Cave Dweller" || B.shardName == "Abyssal Lanternfish")},
+        {id: "R34", predicate: (A, B) => A.shardFamily == "Shulker" && getRarityIndex(A.shardID.slice(0, 1)) >= 3 && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "R43", predicate: (A, B) => A.shardName == "Termite" && B.shardName == "Invisibug"},
+        {id: "R46", predicate: (A, B) => A.shardFamily == "Frog" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "R49", predicate: (A, B) => (A.shardName == "Phanpyre" || A.shardName == "Phanflare") && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "R51", predicate: (A, B) => A.shardName == "Quartzfang" && (B.shardFamily == "Cave Dweller" || B.shardName == "Silentdepth")},
+        {id: "R52", predicate: (A, B) => A.shardFamily == "Cave Dweller" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "R54", predicate: (A, B) => A.shardName == "Python" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 2},
+        {id: "R58", predicate: (A, B) => A.shardFamily == "Bird" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        // Fusion recipes for Epic Shards
+        {id: "E1", predicate: (A, B) => A.shardCategory == "Forest" && A.shardID.slice(0, 1) == "E" && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "E2", predicate: (A, B) => A.shardCategory == "Water" && A.shardID.slice(0, 1) == "E" && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "E3", predicate: (A, B) => A.shardCategory == "Combat" && A.shardID.slice(0, 1) == "E" && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "E4", predicate: (A, B) => A.shardFamily == "Panda" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E5", predicate: (A, B) => A.shardFamily == "Lizard" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E6", predicate: (A, B) => A.shardName == "Crocodile" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E7", predicate: (A, B) => A.shardName == "Bullfrog" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E9", predicate: (A, B) => A.shardName == "King Cobra" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E10", predicate: (A, B) => A.shardName == "Gecko" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E11", predicate: (A, B) => A.shardName == "Eel" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E13", predicate: (A, B) => A.shardName == "Ladybug" && B.shardFamily == "Bug" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E14", predicate: (A, B) => (A.shardName == "Eel" && B.shardName == "Moray Eel") || (A.shardName == "Moray Eel" && B.shardName == "Firefly")},
+        {id: "E16", predicate: (A, B) => A.shardFamily == "Shulker" && getRarityIndex(A.shardID.slice(0, 1)) >= 3 && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "E22", predicate: (A, B) => A.shardName == "Iguana" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E28", predicate: (A, B) => A.shardFamily == "Bird" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "E30", predicate: (A, B) => A.shardName == "Alligator" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E31", predicate: (A, B) => A.shardName == "Praying Mantis" && (B.shardName == "Bezal" || B.shardName == "Cinderbat" || B.shardName == "Flare" || B.shardName == "Lava Flame" || B.shardName == "Fire Eel" || B.shardName == "Bal" || B.shardName == "Flaming Spider")},
+        {id: "E32", predicate: (A, B) => A.shardName == "Moray Eel" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E35", predicate: (A, B) => A.shardName == "Shellwise" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 3},
+        {id: "E36", predicate: (A, B) => A.shardFamily == "Cave Dweller" && getRarityIndex(A.shardID.slice(0, 1)) >= 3 && B.shardFamily == "Cave Dweller" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "E37", predicate: (A, B) => A.shardName == "Firefly" && B.shardFamily == "Dragon"},
+        // Fusion recipes for Legendary Shards
+        {id: "L1", predicate: (A, B) => A.shardCategory == "Forest" && A.shardID.slice(0, 1) == "L" && B.shardFamily == "Elemental" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L2", predicate: (A, B) => A.shardCategory == "Water" && A.shardID.slice(0, 1) == "L" && B.shardFamily == "Elemental" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L3", predicate: (A, B) => A.shardCategory == "Combat" && A.shardID.slice(0, 1) == "L" && B.shardFamily == "Elemental" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L6", predicate: (A, B) => A.shardName == "Caiman" && B.shardCategory == "Combat" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L7", predicate: (A, B) => A.shardName == "Komodo Dragon" && B.shardCategory == "Forest" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L8", predicate: (A, B) => A.shardName == "Leatherback" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L11", predicate: (A, B) => A.shardName == "Sea Serpent" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L12", predicate: (A, B) => A.shardName == "Lapis Skeleton" && (B.shardName == "Ghost" || getRarityIndex(B.shardID.slice(0, 1)) >= 5)},
+        {id: "L13", predicate: (A, B) => A.shardName == "Dragonfly" && B.shardName == "Lunar Moth"},
+        {id: "L18", predicate: (A, B) => A.shardName == "Kraken" && B.shardName == "Kraken"},
+        {id: "L20", predicate: (A, B) => A.shardName == "Kraken" && B.shardCategory == "Water"},
+        {id: "L24", predicate: (A, B) => A.shardName == "Kraken" && B.shardFamily == "Bird"},
+        {id: "L25", predicate: (A, B) => A.shardFamily == "Shulker" && getRarityIndex(A.shardID.slice(0, 1)) >= 4 && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "L29", predicate: (A, B) => A.shardName == "Tortoise" && B.shardCategory == "Water" && getRarityIndex(B.shardID.slice(0, 1)) >= 4},
+        {id: "L31", predicate: (A, B) => A.shardFamily == "Bird" && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "L32", predicate: (A, B) => A.shardName == "Shinyfish" && (B.shardName == "Bezal" || B.shardName == "Cinderbat" || B.shardName == "Flare" || B.shardName == "Lava Flame" || B.shardName == "Fire Eel" || B.shardName == "Bal" || B.shardName == "Flaming Spider")},
+        {id: "L33", predicate: (A, B) => A.shardName == "Power Dragon"},
+        {id: "L34", predicate: (A, B) => A.shardName == "Condor" && getRarityIndex(B.shardID.slice(0, 1)) >= 5},
+        {id: "L36", predicate: (A, B) => A.shardName == "Power Dragon" && B.shardName == "Kraken"},
+        {id: "L39", predicate: (A, B) => A.shardName == "Apex Dragon" && B.shardName == "Kraken"},
+        {id: "L41", predicate: (A, B) => A.shardName == "Sun Fish" && B.shardName == "Sun Fish"},
+        {id: "L42", predicate: (A, B) => A.shardName == "Etherdrake" && B.shardName == "Jormung"},
+        {id: "L44", predicate: (A, B) => A.shardFamily == "Elemental" && A.shardID.slice(0, 1) == "L" && B.shardName == "Galaxy Fish"},
     ];
 }
 
@@ -154,7 +213,7 @@ var checkIfShardIDExists = function(ShardTable, ShardID) {
     var flag = false;
     ShardTable.sorted.forEach(item => {
         if(!item || item == null) return;
-        if(item.shardID == ShardID + "\n") flag = true;
+        if(item.shardID == ShardID) flag = true;
     });
     return flag;
 }
